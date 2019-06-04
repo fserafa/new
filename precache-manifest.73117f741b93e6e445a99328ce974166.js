583e3e77af2f2928760dafab6d3ae847cc764073
Felipe <felipe.seraf@gmail.com>
2019-06-04 14:26:03 -0300

    Updates

---
+++ precache-manifest.73117f741b93e6e445a99328ce974166.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "a291318207ebfd9abc4b",
+    "url": "/new/static/css/main.47da624a.chunk.css"
+  },
+  {
+    "revision": "a291318207ebfd9abc4b",
+    "url": "/new/static/js/main.a2913182.chunk.js"
+  },
+  {
+    "revision": "8ffbd5d8bed79040a0bc",
+    "url": "/new/static/js/1.8ffbd5d8.chunk.js"
+  },
+  {
+    "revision": "a198f4cc6c13f64901b3",
+    "url": "/new/static/js/runtime~main.a198f4cc.js"
+  },
+  {
+    "revision": "05c44179e6d239a39167ebc9e5bb95ed",
+    "url": "/new/static/media/pedra.05c44179.png"
+  },
+  {
+    "revision": "74f9a62add655f0f6b579de7059819e8",
+    "url": "/new/static/media/papel.74f9a62a.png"
+  },
+  {
+    "revision": "570ebdd11409bde000dc5cb8c294afde",
+    "url": "/new/static/media/tesoura.570ebdd1.png"
+  },
+  {
+    "revision": "cb0b200ff600fa42012c9d9a892e0ea1",
+    "url": "/new/static/media/bolso.cb0b200f.jpg"
+  },
+  {
+    "revision": "c82f53337ce8962cd203239eb055de48",
+    "url": "/new/static/media/lula.c82f5333.jpg"
+  },
+  {
+    "revision": "25c19c35007895b45408794f15ccb27d",
+    "url": "/new/index.html"
+  }
+];
